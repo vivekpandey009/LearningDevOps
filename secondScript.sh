215a778fe378fae92fdddb0b8b1068f3db5d7e31
vivekpandey009 <vivek.vidyamandir@gmail.com>
2026-03-30 19:30:33 +0530

    Added substarction feature

--- secondScript.sh
+++ secondScript.sh
@@ -14,9 +14,10 @@ a=7
 b=8
 
 c=$((a+b))
+d=$((a-b))
 
 echo "The addition of $a and $b is $c"
-
+echo "The substraction of $a and $b is $d"
 if ((c%2==0))
 then 
 	echo "The addition is an even number"
